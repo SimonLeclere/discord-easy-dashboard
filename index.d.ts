df9e561ec0ed32d88bab14f6a4c42393c4e8966f
cstef <colin@petit-suisse.fr>
2021-09-21 21:14:43 +0200

    Add Typings

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+import { Client as DiscordClient, Guild } from "discord.js";
+import EventEmitter from "events";
+export default Dashboard;
+export { Options };
+
+declare type Client = DiscordClient & { [key: string]: any };
+
+declare class Dashboard extends EventEmitter {
+    constructor(client: Client, options: Options);
+    config: Config;
+    registerCommand(name: string, description: string, usage: string): void;
+    addTextInput(
+        name: string,
+        description: string,
+        validator: (value: any) => boolean,
+        setter: (client: Client, guild: Guild, value: string) => void,
+        getter: (client: Client, guild: Guild) => string
+    ): void;
+    addBooleanInput(
+        name: string,
+        description: string,
+        setter: (client: Client, guild: Guild, value: boolean) => void,
+        getter: (client: Client, guild: Guild) => boolean
+    ): void;
+    addSelector(
+        name: string,
+        description: string,
+        getSelectorEntries: (client: Client, guild: Guild) => [string, string][],
+        setter: (client: Client, guild: Guild, value: boolean) => void,
+        getter: (client: Client, guild: Guild) => boolean
+    ): void;
+}
+
+declare interface Options {
+    name?: string;
+    description?: string;
+    serverUrl?: string;
+    baseUrl?: string;
+    port?: number;
+    secret: string;
+    logRequests?: boolean;
+}
+
+declare interface Config {
+    baseUrl: string;
+    port: number;
+    secret: string;
+    logRequests: boolean;
+}
