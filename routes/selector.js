58b60ece2c8a93ba9f98697c111384d09c451495
cstef <colin@petit-suisse.fr>
2021-10-28 20:06:36 +0200

    Fix pages not rendering

--- routes/selector.js
+++ routes/selector.js
@@ -4,17 +4,29 @@ const CheckAuth = (req, res, next) =>
 const { Permissions } = require("discord.js");
 
 const Selector = Router().get("/", CheckAuth, async (req, res) => {
-    return await res.render("selector.ejs", {
-        bot: req.client,
-        user: req.user,
-        guilds: req.user.guilds.sort((a, b) => (a.name < b.name ? -1 : Number(a.name > b.name))),
-        is_logged: Boolean(req.session.user),
-        Perms: Permissions,
-        path: req.path,
-        baseUrl: req.dashboardConfig.baseUrl,
-        port: req.dashboardConfig.port,
-        dashboardDetails: req.dashboardDetails,
-    }, (err, html) => res.status(200).send(html));
+    return await res.render(
+        "selector.ejs",
+        {
+            bot: req.client,
+            user: req.user,
+            guilds: req.user.guilds.sort((a, b) =>
+                a.name < b.name ? -1 : Number(a.name > b.name)
+            ),
+            is_logged: Boolean(req.session.user),
+            Perms: Permissions,
+            path: req.path,
+            baseUrl: req.dashboardConfig.baseUrl,
+            port: req.dashboardConfig.port,
+            dashboardDetails: req.dashboardDetails,
+        },
+        (err, html) => {
+            if (err) {
+                res.status(500).send(err.message);
+                return console.error(err);
+            }
+            res.status(200).send(html);
+        }
+    );
 });
 
 module.exports.Router = Selector;
