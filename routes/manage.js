1cfcc32656279c86534400b68768666abf7a47e7
cstef <53212129+cstefFlexin@users.noreply.github.com>
2021-12-30 16:27:59 +0100

    Remove useless console.warn

--- routes/manage.js
+++ routes/manage.js
@@ -9,13 +9,8 @@ const Server = Router()
 
         const member = await guild.members.fetch(req.user.id);
         if (!member || !member.permissions.has("MANAGE_GUILD")) return res.redirect("/selector");
-        let file = req.dashboardConfig.theme["guild"];
-        if (!file) {
-            console.warn(
-                `WARNING: No key found in the theme object for the guild route, falling back to the default one`
-            );
-            file = "guild.ejs";
-        }
+        let file = req.dashboardConfig.theme["guild"] || "guild.ejs";
+
         return await res.render(
             file,
             {
