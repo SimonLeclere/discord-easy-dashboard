7aeeb60dbbdd70520ffe727941bbc7f7943082d9
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-03-09 18:44:53 +0530

    res

--- routes/change.js
+++ routes/change.js
@@ -5,33 +5,11 @@ let file = ""
 const Change = Router().get("/", [CheckAuth], async (req, res) => {
 
     if (req.dashboardConfig.mode[req.user.id] == "light") {
-        req.dashboardConfig.mode[req.user.id] = "dark";
         file = req.dashboardConfig.theme["home"] || "index.ejs";
     } else {
-        file = req.dashboardConfig.theme["homel"] || "indexl.ejs";
         req.dashboardConfig.mode[req.user.id] = "light";
     }
-    return await res.render(
-        file,
-        {
-            bot: req.client,
-            user: req.user,
-            is_logged: Boolean(req.session.user),
-            dashboardDetails: req.dashboardDetails,
-            dashboardConfig: req.dashboardConfig,
-            baseUrl: req.dashboardConfig.baseUrl,
-            port: req.dashboardConfig.port,
-            hasClientSecret: Boolean(req.dashboardConfig.secret),
-            commands: req.dashboardCommands,
-        },
-        (err, html) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return console.error(err);
-            }
-            res.status(200).send(html);
-        }
-    );
+    res.redirect("/");
 });
 
 module.exports.Router = Change;
