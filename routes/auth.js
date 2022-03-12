3fb56b595e72615873012c5f1d8615465ec95b86
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-03-12 22:27:22 +0530

    code update

--- routes/auth.js
+++ routes/auth.js
@@ -63,6 +63,7 @@ const Auth = Router()
             });
             req.dashboardEmit("newUser", req.session.user);
             res.status(200).redirect("/");
+            req.dashboardConfig.mode[userData.infos.id] = "dark"
         } else {
             res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${req.client?.user?.id}&scope=identify%20guilds&response_type=code&redirect_uri=${encodeURIComponent(`${req.dashboardConfig.baseUrl}${req.dashboardConfig.noPortIncallbackUrl ? '' : ':' + req.dashboardConfig.port}/auth/login`)}`);
         }
