11281599de56957ff4d6f9287a586cd1996add58
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-03-13 18:36:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ class Dashboard extends EventEmitter {
                 : require(join(__dirname, "themes", "dark")),
             mode: this._mode,
             session: options?.session || null,
-            permissions: options?.permissions || "ADMINISTRATOR"
+            permissions: options?.permissions || [Permissions.FLAGS.ADMINISTRATOR]
         };
 
         if (!this.config.secret)
