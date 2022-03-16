79503c806c625a3e323fd6d5f9c67945f3e3023a
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-03-16 19:07:31 +0530

    final commit

--- routes/commands.js
+++ routes/commands.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const CheckAuth = (req, res, next) =>
     req.session.user ? next() : res.status(401).redirect("/auth/login");
 var name = "/commands";
+const { Permissions } = require("discord.js");
+
 const Commands = Router().get("/", [CheckAuth], function (req, res) {
     if (req.dashboardCommands.length === 0) return res.redirect("/");
     let file = req.dashboardConfig.theme["ahqcmd"] || "commands_list.ejs";
@@ -9,21 +11,30 @@ const Commands = Router().get("/", [CheckAuth], function (req, res) {
     if (req.dashboardConfig.mode[req.user.id] == "light") {
         file = req.dashboardConfig.theme["ahqcmdl"] || "commands_listl.ejs";
     }
-    res.status(200).render(file, {
-        bot: req.client,
-        user: req.user,
-        is_logged: Boolean(req.session.user),
-        dashboardDetails: req.dashboardDetails,
-        dashboardConfig: req.dashboardConfig,
-        baseUrl: req.dashboardConfig.baseUrl,
-        port: req.dashboardConfig.port,
-        hasClientSecret: Boolean(req.dashboardConfig.secret),
-        commands: req.dashboardCommands,
-        mode: req.dashboardConfig.mode,
-        guilds: req.user.guilds.sort((a, b) =>
-        a.name < b.name ? -1 : Number(a.name > b.name)
-        ),
-    });
+    return await res.render(
+        file,
+        {
+            bot: req.client,
+            user: req.user,
+            guilds: req.user.guilds.sort((a, b) =>
+                a.name < b.name ? -1 : Number(a.name > b.name)
+            ),
+            is_logged: Boolean(req.session.user),
+            Perms: Permissions,
+            path: req.path,
+            baseUrl: req.dashboardConfig.baseUrl,
+            port: req.dashboardConfig.port,
+            dashboardDetails: req.dashboardDetails,
+            dashboardConfig: req.dashboardConfig,
+        },
+        (err, html) => {
+            if (err) {
+                res.status(500).send(err.message);
+                return console.error(err);
+            }
+            res.status(200).send(html);
+        }
+    );
 })
 .get("/:guildID", [CheckAuth], async (req, res) => {
     const guild = req.client.guilds.cache.get(req.params.guildID);
