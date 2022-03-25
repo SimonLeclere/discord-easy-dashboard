d43802137265bde57031a93c767dd08c57dd78b1
TesseractB <81273859+TesseractB@users.noreply.github.com>
2022-03-25 01:20:48 -0400

    Uncomment Modals

--- index.d.ts
+++ index.d.ts
@@ -68,44 +68,44 @@ declare interface Config {
     permissions: Permissions[]
 }
 
-// declare class Modal {
-//     constructor(options: ModalOptions)
-//     addTextInput(
-//         name: string,
-//         description: string,
-//         validator: (value: any) => boolean,
-//         setter: (client: Client, guild: Guild, value: string) => void,
-//         getter: (client: Client, guild: Guild) => string | Promise<string>
-//     ): void;
-//     addBooleanInput(
-//         name: string,
-//         description: string,
-//         setter: (client: Client, guild: Guild, value: boolean) => void,
-//         getter: (client: Client, guild: Guild) => boolean | Promise<boolean>
-//     ): void;
-//     addSelector(
-//         name: string,
-//         description: string,
-//         getSelectorEntries: (
-//             client: Client,
-//             guild: Guild
-//         ) => [string, string][] | Promise<[string, string][]>,
-//         setter: (client: Client, guild: Guild, value: boolean) => void,
-//         getter: (client: Client, guild: Guild) => boolean | Promise<boolean>
-//     ): void;
+declare class Modal {
+    constructor(options: ModalOptions)
+    addTextInput(
+        name: string,
+        description: string,
+        validator: (value: any) => boolean,
+        setter: (client: Client, guild: Guild, value: string) => void,
+        getter: (client: Client, guild: Guild) => string | Promise<string>
+    ): void;
+    addBooleanInput(
+        name: string,
+        description: string,
+        setter: (client: Client, guild: Guild, value: boolean) => void,
+        getter: (client: Client, guild: Guild) => boolean | Promise<boolean>
+    ): void;
+    addSelector(
+        name: string,
+        description: string,
+        getSelectorEntries: (
+            client: Client,
+            guild: Guild
+        ) => [string, string][] | Promise<[string, string][]>,
+        setter: (client: Client, guild: Guild, value: boolean) => void,
+        getter: (client: Client, guild: Guild) => boolean | Promise<boolean>
+    ): void;
 
-//     addColorInput(
-//         name: string,
-//         description: string,
-//         setter: (client: Client, guild: Guild, value: string) => void,
-//         getter: (client: Client, guild: Guild) => string | Promise<string>
-//     ): void;
+    addColorInput(
+        name: string,
+        description: string,
+        setter: (client: Client, guild: Guild, value: string) => void,
+        getter: (client: Client, guild: Guild) => string | Promise<string>
+    ): void;
 
-// }
+}
 
-// declare interface ModalOptions {
-//     title?: string;
-//     openButtonLabel?: string;
-//     closeButtonLabel?: string;
-//     saveButtonLabel?: string;
-// }
+declare interface ModalOptions {
+    title?: string;
+    openButtonLabel?: string;
+    closeButtonLabel?: string;
+    saveButtonLabel?: string;
+}
