45d3e04d6f5ef3ec8d0ee3cda6312ec0b85f8c79
Simon Leclere <simon-leclere@orange.fr>
2023-04-17 19:05:50 +0200

    Lint

--- routes/custom.js
+++ routes/custom.js
@@ -1,33 +1,33 @@
-const { Router } = require('express');
+const { Router } = require('express')
 
-const Commands = Router().get('/*', function(req, res) {
-	const path = req.baseUrl.split('/').pop();
-	if (!req.dashboardConfig.theme[path]) {
-		const file = req.dashboardConfig.theme['404'] || '404.ejs';
-		return res.status(404).render(file, {
-			bot: req.client,
-			user: req.user,
-			is_logged: Boolean(req.session.user),
-			dashboardDetails: req.dashboardDetails,
-			dashboardConfig: req.dashboardConfig,
-			baseUrl: req.dashboardConfig.baseUrl,
-			port: req.dashboardConfig.port,
-			hasClientSecret: Boolean(req.dashboardConfig.secret),
-			commands: req.dashboardCommands,
-		});
-	}
-	res.status(200).render(req.dashboardConfig.theme[path], {
-		bot: req.client,
-		user: req.user,
-		is_logged: Boolean(req.session.user),
-		dashboardDetails: req.dashboardDetails,
-		dashboardConfig: req.dashboardConfig,
-		baseUrl: req.dashboardConfig.baseUrl,
-		port: req.dashboardConfig.port,
-		hasClientSecret: Boolean(req.dashboardConfig.secret),
-		commands: req.dashboardCommands,
-	});
-});
-module.exports.Router = Commands;
+const Commands = Router().get('/*', function (req, res) {
+  const path = req.baseUrl.split('/').pop()
+  if (!req.dashboardConfig.theme[path]) {
+    const file = req.dashboardConfig.theme['404'] || '404.ejs'
+    return res.status(404).render(file, {
+      bot: req.client,
+      user: req.user,
+      is_logged: Boolean(req.session.user),
+      dashboardDetails: req.dashboardDetails,
+      dashboardConfig: req.dashboardConfig,
+      baseUrl: req.dashboardConfig.baseUrl,
+      port: req.dashboardConfig.port,
+      hasClientSecret: Boolean(req.dashboardConfig.secret),
+      commands: req.dashboardCommands
+    })
+  }
+  res.status(200).render(req.dashboardConfig.theme[path], {
+    bot: req.client,
+    user: req.user,
+    is_logged: Boolean(req.session.user),
+    dashboardDetails: req.dashboardDetails,
+    dashboardConfig: req.dashboardConfig,
+    baseUrl: req.dashboardConfig.baseUrl,
+    port: req.dashboardConfig.port,
+    hasClientSecret: Boolean(req.dashboardConfig.secret),
+    commands: req.dashboardCommands
+  })
+})
+module.exports.Router = Commands
 
-module.exports.name = '/*';
+module.exports.name = '/*'
